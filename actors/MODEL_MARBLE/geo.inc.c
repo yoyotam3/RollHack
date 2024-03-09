f2e77d21c3d1d10e1ccc28e7d95ef0a9f5d1cc57
yoyotam3 <theredstonelord123@gmail.com>
2024-03-09 18:04:05 +1100

    Removed marble model

--- actors/MODEL_MARBLE/geo.inc.c
+++
@@ -1,9 +0,0 @@
-#include "src/game/envfx_snow.h"
-
-const GeoLayout MODEL_MARBLE_GEO[] = {
-	GEO_NODE_START(),
-	GEO_OPEN_NODE(),
-		GEO_DISPLAY_LIST(LAYER_OPAQUE, MODEL_MARBLE_Icosphere_mesh_layer_1),
-	GEO_CLOSE_NODE(),
-	GEO_END(),
-};
